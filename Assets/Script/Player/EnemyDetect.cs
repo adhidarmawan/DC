cf1e5fac72c8f99d1599a1dd00fca8a2b5417053
adhidarmawan <rahmawan.sutjiadi@gmail.com>
2016-01-29 23:58:53 +0700

    first

---
+++ Assets/Script/Player/EnemyDetect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDetect : MonoBehaviour {
+
+	public Collision PlayerRadius;
+	public Collider PlayerCollider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log("collison");
+	}
+
+	void OnColliderEnter(Collider collider){
+		Debug.Log("collider");
+	}
+
+}
